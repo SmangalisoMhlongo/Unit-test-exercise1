c8a75f4b76e395a2c094d26cb18017c495a404c6
SmangalisoMhlongo <smangalisomhlongo187@gmail.com>
2025-10-01 14:16:25 +0200

    Done

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 def factorial(n):
     """Calculates the factorial of a non-negative integer."""
     # TODO: Implement the logic
-    pass 
+    if n < 0:
+        return "Enter a non negative number"
+    fact = 1
+    
+    for i in range(1,n+1):
+        fact = fact * i
+
+    return fact     
+
 
 def fibonacci(n):
     """Returns the nth number in the Fibonacci sequence."""
     # TODO: Implement the logic
-    return None
+    #the default positions position sequence starts at 1
+    ls = [0,1]
+    if n < 2:
+        return n
+    #should be inclusive of n
+    for i in range(2,n+1):
+       #using indexing to calcualte
+        ls.append(ls[i - 1] + ls[i-2])
+    return ls[-1]
 
 def fizzbuzz(n):
     """
@@ -16,4 +32,17 @@ def fizzbuzz(n):
     - Multiples of both 3 and 5 are "FizzBuzz"
     """
     # TODO: Implement the logic
-    return []
+    ls = []
+    for i in range(1,n+1):
+        if i%3 == 0 and i%5 == 0:
+            ls.append("FizzBuzz")
+        elif i%3 == 0:
+            ls.append("Fizz")
+        elif i%5 == 0:
+            ls.append("Buzz")
+        else:
+             ls.append(i)
+    
+    return ls
+
+print(factorial(5))
